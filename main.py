e22f5392b8ade5879bf088804a78e4ef737eb28e
Magnefficient <95318368+Magnefficient@users.noreply.github.com>
2021-11-30 16:10:40 -0600

    Add files via upload
    in this program  forgive i'm new but    i have  made a  program that will  spell check and also let you know how many letters were mispelled

---
+++ main.py
@@ -0,0 +1,35 @@
+choiceGroup = []
+studentGroup =[]
+mispelled = 0
+choiceOfWord = input("ask the other user what  they want to type")
+for x in choiceOfWord:
+    choiceGroup.append(x)
+
+#make sure that the students guess is the same size   as  the  correct word
+
+letter = input("type the word your teacher wants you to spell")
+for s in letter:
+    studentGroup.append(s)
+
+
+for t in range(len(choiceGroup)):
+    if choiceGroup[t] != studentGroup[t]: """# fixed the problem in which the incorect number of letters mispelled wwas displayed by linking the current  
+                                          value of t to the index of the two list this  allowed both letters to be analyzed by the computer  at the same time """
+        mispelled += 1
+
+
+        """ changed to  plus equal 1 from plus 1 
+        so the computer would stop to evaluate the variable and not say 
+        """
+
+
+
+
+
+print(mispelled , "letter(s) mispelled")
+
+
+
+
+
+
